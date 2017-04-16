908f3e686e1e1cd6ab53f89a5349229ff7dc60de
Qwebeck5 <Qwebeck5@gmail.com>
2017-04-16 22:48:08 +0300

    Add compare with form value for all elements of array

--- public/scripts/search.js
+++ public/scripts/search.js
@@ -1,9 +1,6 @@
 var $button = $("#searchButton")
 var $form = $("#bookName")
 
-$button.click(function(){
-    console.log($form.val())
-})
 
 
 
@@ -12,4 +9,32 @@ $button.click(function(){
 $.ajax({
     url:"/search",
     method:"POST"
-}).then(function(res){console.log(res[0].name)})
+}).then(function(res)
+{
+    var books = res;
+   
+//    var newBooks = books.filter(function(book){
+//         return  books.name==$form.val()
+//     })
+
+
+    
+$button.click(function(){
+console.log(res[0].name)
+
+    console.log($form.val())
+
+    books.forEach(function(book){
+   if(book.name==$form.val()){
+       console.log('ok')
+   }
+
+})
+    
+    // console.log(newBooks)
+
+
+
+})
+
+})
