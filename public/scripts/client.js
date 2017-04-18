aef16828ce7559c1dd2af4a4f737355b4164a48d
Qwebeck5 <Qwebeck5@gmail.com>
2017-04-18 19:32:17 +0300

    Now files appear on page

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -14,11 +14,11 @@ function renderBooks() {
 
             $bookTemplate.find("[data-book-name]").text(book.name);
             $bookTemplate.find("[data-book-author]").text(book.author);
-            // $bookTemplate.find("[data-book-image]").attr("src", book.picture);
+            $bookTemplate.find("[data-book-image]").attr("src", book.picture);
             $bookTemplate.find("[data-book-tags]").text(book.tags);
             $bookTemplate.find("[data-book-link]").attr("href", "/book/" + book.id);
-            //   $bookTemplate.attr("style","display:block")
-            // $booksWrapper.append($bookTemplate);
+           
+            $booksWrapper.append($bookTemplate);
         });
     })
 }
--- public/scripts/search.js
+++ public/scripts/search.js
@@ -1,17 +1,5 @@
 var $button = $("#searchButton")
-bookAppear()
 
-
-function bookAppear(book) {
-            var $bookTemplate = $("#template > div").clone();
-
-            $bookTemplate.find("[data-book-name]").text(book.name);
-            $bookTemplate.find("[data-book-author]").text(book.author);
-            $bookTemplate.find("[data-book-image]").attr("src", book.picture);
-            $bookTemplate.find("[data-book-tags]").text(book.tags);
-            $bookTemplate.find("[data-book-link]").attr("href", "/book/" + book.id);
-           
-            $booksWrapper.append($bookTemplate);
 
 
 
@@ -33,57 +21,19 @@ $.ajax({
 
  var $booksWrapper = $("#books");
 
-         books.forEach(function(){bookAppear()})
-         //(function bookAppear(book) {
-        //     var $bookTemplate = $("#template > div").clone();
+        books.forEach(function bookAppear(book) {
+            var $bookTemplate = $("#template > div").clone();
 
-        //     $bookTemplate.find("[data-book-name]").text(book.name);
-        //     $bookTemplate.find("[data-book-author]").text(book.author);
-        //     $bookTemplate.find("[data-book-image]").attr("src", book.picture);
-        //     $bookTemplate.find("[data-book-tags]").text(book.tags);
-        //     $bookTemplate.find("[data-book-link]").attr("href", "/book/" + book.id);
+            $bookTemplate.find("[data-book-name]").text(book.name);
+            $bookTemplate.find("[data-book-author]").text(book.author);
+            $bookTemplate.find("[data-book-image]").attr("src", book.picture);
+            $bookTemplate.find("[data-book-tags]").text(book.tags);
+            $bookTemplate.find("[data-book-link]").attr("href", "/book/" + book.id);
            
-        //     $booksWrapper.append($bookTemplate);
+            $booksWrapper.append($bookTemplate);
         });
 
 
 
 
-    })}
-// {
-//     var books = res;
-   
-//    var newBooks = books.filter(function(book){
-//         return  books.name==$form.val()
-//     })
-
-
-    
-// console.log(res[0].name)
-
-//     console.log($form.val())
-// var newBooks = books.filter(function(book){
-//     return book.name == $form.val()
-// })
-
-            
-
-//           newBooks.forEach(function(book) {
-//             var $bookTemplate = $("#template > div").clone();
-
-//             $bookTemplate.find("[data-book-name]").text(book.name);
-//             $bookTemplate.find("[data-book-author]").text(book.author);
-//             $bookTemplate.find("[data-book-image]").attr("src", book.picture);
-//             $bookTemplate.find("[data-book-tags]").text(book.tags);
-//             $bookTemplate.find("[data-book-link]").attr("href", "/book/" + book.id);
-
-//             $booksWrapper.append($bookTemplate);
-//         });
-    
-//     console.log(newBooks)
-
-
-
-// })
-
-// })
+    })})
